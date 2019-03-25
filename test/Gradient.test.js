1e99384244bc18638045fd745e97635316923540
Nikita Belous <belousnikita@gmail.com>
2019-03-25 21:20:17 +0200

    Range tests

--- test/Gradient.test.js
+++ test/Gradient.test.js
@@ -24,6 +24,27 @@ test('Colors creation 3', () => {
     expect(palette.colors[1].hex).toBe("#0000ff");
     expect(palette.colors[2].hex).toBe("#008000");
 })
-test('Throw', () => {
+test('Throw error', () => {
     expect(() => new Palette('gradient', 'red', 'blue', '1231233')).toThrowError();
+})
+test('Setting range 0 - 99', () => {
+    const palette = new Palette('gradient', 'red', 'blue', 'green');
+    expect(palette.min).toBe(0);
+    expect(palette.max).toBe(2);
+    palette.setPointsRange(0, 99);
+    expect(palette.min).toBe(0);
+    expect(palette.max).toBe(99);
+    expect(palette.colors.length).toBe(99 || 100);
+})
+test('Setying range 5 - 10', () => {
+    const palette = new Palette('gradient', 'red', 'blue', 'green');
+    palette.setPointsRange(5, 10);
+    expect(palette.colors.length).toBe(5);
+    expect(palette.min).toBe(5);
+    expect(palette.max).toBe(10);
+    expect(palette.colors.length).toBe(5 || 6);
+})
+test('Setting range 4 - 4 should throw Error', () => {
+    const palette = new Palette('gradient', 'red', 'blue', 'green');
+    expect(() => palette.setPointsRange(4, 4)).toThrowError();
 })
